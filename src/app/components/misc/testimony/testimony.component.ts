ae0682a49a19a3c5182c522e7b05e16f8a6b23b7
Alabi Sodiq <adisco4420@gmail.com>
2020-05-08 17:11:37 +0100

    update testimony

--- src/app/components/misc/testimony/testimony.component.ts
+++ src/app/components/misc/testimony/testimony.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimony.component.scss']
 })
 export class TestimonyComponent implements OnInit {
-
+  testimonies = [
+    {name: 'Grace Thato Ndumiso', content: 'Thanks to Dailycryptomine, I invest and I made my profit successful'},
+    {name: 'Velisphomiya', content: 'This Brooker is really legit, I invested and got my profit without stress'},
+    {name: 'Darmain Grayson', content: `Dailycryptomine as really change my financial
+      status thanks to them I invested and earned successful`},
+    {name: 'Hagos Trip', content: `I'm really happy today the website as change my
+        financial life, you should try it and do not doubt I've gain alot profit, thanks to dailycryptomine`},
+    {name:'Sarah Lauren smith', content: 'This is the best website i have invested on i made huge profit within a short time'}
+  ]
   constructor() { }
 
   ngOnInit() {
